0d08f878434fd5a2cc1f179aa9cf0747e8ef438a
Jerzy Baran <1chmsid@gmail.com>
2015-02-15 22:22:43 +0000

    tested the Deck class

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,4 +1,15 @@
+#include <stdio.h>
+
+#include "Deck.hpp"
+
 int main(){
+
+	Deck deck(CARDS_26);
+
+	printf("Cards in deck: %i\n", deck.getNumCardsInDeck());
+	Card c = deck.drawCard();
+	printf("Removed: %s\n", c.wordSuit().c_str());
+	printf("Cards in deck: %i\n", deck.getNumCardsInDeck());
 
     return 0;
 }
